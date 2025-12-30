b5d24d1872cdc9cc74595dfc65582cec0ae5f826
Bizgojo <72152082+Bizgojo@users.noreply.github.com>
2025-12-30 16:38:26 -0500

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,7 @@
 // UI Components
 export { Logo, LogoStacked, LogoIcon } from './ui/Logo';
 export { Header } from './ui/Header';
-export { StoryCard, type Story } from './ui/StoryCard';
+export { StoryCard } from './ui/StoryCard';
 export { CreditStatus } from './ui/CreditStatus';
 export { DurationFilter, filterByDuration, getDurationCounts, type DurationFilterValue } from './ui/DurationFilter';
 export { StoryModal } from './ui/Modal';
