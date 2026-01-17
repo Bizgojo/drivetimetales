859af81fb46c4ffb4c739af5ce9b4cb7dcaeed83
Bizgojo <72152082+Bizgojo@users.noreply.github.com>
2026-01-17 16:24:04 -0500

    Remove duplicate RecommendedForYou

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -110,7 +110,6 @@ export default function HomePage() {
         <ContinueListening />
         <NewReleases />
         <RecommendedForYou />
-        <RecommendedForYou />
       </main>
     </div>
   )
