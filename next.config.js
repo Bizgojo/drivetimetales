a51f2b047afd2e153d288e806fcb92727bfa1b86
Bizgojo <72152082+Bizgojo@users.noreply.github.com>
2026-03-11 11:54:31 -0400

    fix: add sharp to webpack server externals to fix Vercel build

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ module.exports = {
   experimental: {
     serverComponentsExternalPackages: ['ws', 'bufferutil', 'utf-8-validate', 'ffmpeg-static', 'fluent-ffmpeg', 'sharp'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'sharp']
+    }
+    return config
+  },
 }
