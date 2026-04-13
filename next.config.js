40c7ee9c5de799e38f026bee43b9ba277aa2ebc0
Bizgojo <72152082+Bizgojo@users.noreply.github.com>
2026-04-13 15:28:16 -0400

    fix: outputFileTracingIncludes to bundle ffmpeg binary into serverless functions

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ module.exports = {
   typescript: { ignoreBuildErrors: true },
   experimental: {
     serverComponentsExternalPackages: ['ws', 'bufferutil', 'utf-8-validate', 'fluent-ffmpeg', 'sharp'],
+    outputFileTracingIncludes: {
+      '/api/asc3/render-final-mix': ['./node_modules/ffmpeg-static/**/*'],
+      '/api/landing/render-mix': ['./node_modules/ffmpeg-static/**/*'],
+    },
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
